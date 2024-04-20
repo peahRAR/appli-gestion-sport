3e521d24bdc924473afe52f38aa32dc59215e49a
peahRAR <padecallonne@gmail.com>
2024-04-20 16:52:00 +0200

    ajout d'un decorator public dans app controller

--- back-end/src/app.controller.ts
+++ back-end/src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { User } from './users/users.entity';
+import { Public } from './decorators/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   initApp(): Promise<User> {
     return this.appService.initApp();

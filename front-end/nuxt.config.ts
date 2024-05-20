e013f5a0f509acc945798254ce12697df6854d75
peahRAR <padecallonne@gmail.com>
2024-05-20 14:03:24 +0200

    fix bug 404

--- front-end/nuxt.config.ts
+++ front-end/nuxt.config.ts
@@ -1,7 +1,8 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   app: {
-    baseURL: process.env.CI ? "/storage.googleapis.com/frontend-app-mma" : "/",
+    baseURL: process.env.CI ? "/frontend-app-mma" : "/",  // Base URL de l'application
+    buildAssetsDir: process.env.CI ? "/frontend-app-mma/_nuxt/" : "/_nuxt/",  // Dossier des assets de build
   },
 
   ssr: false,
@@ -19,6 +20,17 @@ export default defineNuxtConfig({
       resetKey: process.env.REINITIALIZATIONKEY,
       frontVersion: "Bêta",
       backVersion: "1.0",
+    },
+  },
+  nitro: {
+    prerender: {
+      routes: [
+        '/',
+        '/terms',
+        '/admin',
+        '/reset-password',
+        // Ajouter d'autres routes nécessaires
+      ],
     },
   },
 });

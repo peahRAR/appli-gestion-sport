04bb2c08f022aadcb16f4b955043430cfb252e2b
peahRAR <padecallonne@gmail.com>
2024-05-25 14:00:45 +0200

    Fix pb timezone

--- front-end/components/CreateCourseForm.vue
+++ front-end/components/CreateCourseForm.vue
@@ -5,41 +5,69 @@
       <!-- Name Event -->
       <div class="flex flex-col">
         <label for="title" class="font-semibold">Titre du cours</label>
-        <input type="text" v-model="newCourse.name_event" id="title"
-          class="border border-gray-300 bg-gray-200 px-4 py-2 rounded-md" />
+        <input
+          type="text"
+          v-model="newCourse.name_event"
+          id="title"
+          class="border border-gray-300 bg-gray-200 px-4 py-2 rounded-md"
+        />
       </div>
       <!-- Overview -->
       <div class="flex flex-col">
-        <label for="description" class="font-semibold">Description du cours</label>
-        <textarea v-model="newCourse.overview" id="description"
-          class="border border-gray-300 bg-gray-200 px-4 py-2 rounded-md"></textarea>
+        <label for="description" class="font-semibold"
+          >Description du cours</label
+        >
+        <textarea
+          v-model="newCourse.overview"
+          id="description"
+          class="border border-gray-300 bg-gray-200 px-4 py-2 rounded-md"
+        ></textarea>
       </div>
       <!-- Date -->
       <div class="flex flex-col">
         <label class="font-semibold" for="date">Date et heure de début</label>
-        <input type="datetime-local" v-model="newCourse.date_event" id="date"
-          class="border border-gray-300 bg-gray-200 px-4 py-2 rounded-md" />
+        <input
+          type="datetime-local"
+          v-model="newCourse.date_event"
+          id="date"
+          class="border border-gray-300 bg-gray-200 px-4 py-2 rounded-md"
+        />
       </div>
       <!-- Coach -->
       <div class="flex flex-col">
         <label class="font-semibold" for="coach">Nom du coach</label>
-        <input type="text" v-model="newCourse.coach" id="coach"
-          class="border border-gray-300 bg-gray-200 px-4 py-2 rounded-md" />
+        <input
+          type="text"
+          v-model="newCourse.coach"
+          id="coach"
+          class="border border-gray-300 bg-gray-200 px-4 py-2 rounded-md"
+        />
       </div>
       <!-- Duration -->
       <div class="flex flex-col">
         <label class="font-semibold" for="duration">Durée du cours (en minutes)</label>
-        <input type="number" v-model="newCourse.duration" id="duration"
-          class="border border-gray-300 bg-gray-200 px-4 py-2 rounded-md" />
+        <input
+          type="number"
+          v-model="newCourse.duration"
+          id="duration"
+          class="border border-gray-300 bg-gray-200 px-4 py-2 rounded-md"
+        />
       </div>
       <!-- Total places -->
       <div class="flex flex-col">
         <label class="font-semibold" for="totalSeats">Nombre de places total disponibles</label>
-        <input type="number" v-model="newCourse.totalPlaces" id="totalSeats"
-          class="border border-gray-300 bg-gray-200 px-4 py-2 rounded-md" />
+        <input
+          type="number"
+          v-model="newCourse.totalPlaces"
+          id="totalSeats"
+          class="border border-gray-300 bg-gray-200 px-4 py-2 rounded-md"
+        />
       </div>
       <!-- Submit Button -->
-      <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+      <button
+        type="submit"
+        class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+      >
         Créer le cours
       </button>
     </form>
@@ -58,35 +86,20 @@ export default {
         duration: 0,
         totalPlaces: 0,
       },
-      utcDateTime: ''
     };
   },
   methods: {
-    convertToUTC(localDateTime) {
-      const localDate = new Date(localDateTime);
-      const utcDate = new Date(localDate.getTime() + localDate.getTimezoneOffset() * 60000);
-      return utcDate.toISOString();
-    },
-
     submitForm() {
-      const formattedCourse = { ...this.newCourse };
-      formattedCourse.date_event = this.convertToUTC(this.newCourse.date_event);
-
-      console.log('Date en UTC à envoyer :', formattedCourse.date_event); 
-      this.$emit("create", formattedCourse);
-
-      // Reset the form
+      this.$emit("create", this.newCourse);
       this.newCourse = {
-        name_event: "",
-        overview: "",
+        title: "",
+        description: "",
+        totalSeats: "",
         date_event: "",
         coach: "",
-        duration: 0,
-        totalPlaces: 0,
+        duration: "",
       };
-      this.utcDateTime = '';
-    }
-
-  }
+    },
+  },
 };
 </script>

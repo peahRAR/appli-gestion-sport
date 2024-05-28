14e475b3d5f00418770aa0bb8a8cd89016576cdf
peahRAR <padecallonne@gmail.com>
2024-05-28 16:54:38 +0200

    try to fix credential probleme with configSerice in useFactory

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, DynamicModule, OnModuleInit } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { MailerModule } from '@nestjs-modules/mailer';
@@ -15,71 +15,81 @@ import { CronjobsModule } from './cronjobs/cronjobs.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-@Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
-        type: 'postgres',
-        host: configService.get<string>('DATABASE_HOST'),
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USERNAME'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
-        autoLoadEntities: true,
-        entities: [User, ListsMember],
-        ssl: {
-          rejectUnauthorized: false,
-          ca: configService.get<string>('DATABASE_SSL_CA'),
-        },
-        synchronize: configService.get<boolean>('TYPEORM_SYNC'),
-      }),
-    }),
-    MailerModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        transport: {
-          host: configService.get<string>('SMTP_HOST'),
-          port: configService.get<number>('SMTP_PORT'),
-          secure: false,
-          auth: {
-            user: configService.get<string>('SMTP_USER'),
-            pass: configService.get<string>('SMTP_PASS'),
-          },
-        },
-        defaults: {
-          from: '"Association MMA Baisieux" <no-reply@mmabaisieux.fr>',
-        },
-        template: {
-          dir: process.cwd() + '/templates/email/',
-          adapter: new HandlebarsAdapter(),
-          options: {
-            strict: false,
-          },
-        },
-      }),
-    }),
-    UsersModule,
-    EventsModule,
-    ListsMembersModule,
-    AuthModule,
-    AdminModule,
-    AlertModule,
-    CronjobsModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {
+@Module({})
+export class AppModule implements OnModuleInit {
   constructor(private configService: ConfigService) { }
 
   async onModuleInit() {
     const databaseHost = this.configService.get<string>('DATABASE_HOST');
     console.log(`LOG - Database Host: ${databaseHost}`);
+  }
+
+  static forRoot(secrets: Record<string, any>): DynamicModule {
+    return {
+      module: AppModule,
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          load: [
+            () => ({
+              ...secrets,
+            }),
+          ],
+        }),
+        TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
+            type: 'postgres',
+            host: configService.get<string>('DATABASE_HOST'),
+            port: configService.get<number>('DATABASE_PORT'),
+            username: configService.get<string>('DATABASE_USERNAME'),
+            password: configService.get<string>('DATABASE_PASSWORD'),
+            database: configService.get<string>('DATABASE_NAME'),
+            autoLoadEntities: true,
+            entities: [User, ListsMember],
+            ssl: {
+              rejectUnauthorized: false,
+              ca: configService.get<string>('DATABASE_SSL_CA'),
+            },
+            synchronize: configService.get<boolean>('TYPEORM_SYNC'),
+          }),
+        }),
+        MailerModule.forRootAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: async (configService: ConfigService) => ({
+            transport: {
+              host: configService.get<string>('SMTP_HOST'),
+              port: configService.get<number>('SMTP_PORT'),
+              secure: false,
+              auth: {
+                user: configService.get<string>('SMTP_USER'),
+                pass: configService.get<string>('SMTP_PASS'),
+              },
+            },
+            defaults: {
+              from: '"Association MMA Baisieux" <no-reply@mmabaisieux.fr>',
+            },
+            template: {
+              dir: process.cwd() + '/templates/email/',
+              adapter: new HandlebarsAdapter(),
+              options: {
+                strict: false,
+              },
+            },
+          }),
+        }),
+        UsersModule,
+        EventsModule,
+        ListsMembersModule,
+        AuthModule,
+        AdminModule,
+        AlertModule,
+        CronjobsModule,
+      ],
+      controllers: [AppController],
+      providers: [AppService],
+    };
   }
 }
--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,19 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import loadSecrets from './config/configuration';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const secrets = await loadSecrets();
-
-  const app = await NestFactory.create(AppModule);
-
-  // Inject secrets into the ConfigService
-  const configService = app.get(ConfigService);
-  Object.keys(secrets).forEach(key => {
-    configService.set(key, secrets[key]);
-  });
-
+  const app = await NestFactory.create(AppModule.forRoot(secrets));
   app.enableCors();
   await app.listen(8080);
 }

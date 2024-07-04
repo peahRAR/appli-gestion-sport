c1a8f2ef73696b3742815a4045f9f6cafdb93d07
peahRAR <padecallonne@gmail.com>
2024-07-04 21:03:23 +0200

    ajout du scheduleModule, pour les cronJobs dans le app module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -56,7 +56,8 @@ export class AppModule implements OnModuleInit {
             },
             synchronize: configService.get<boolean>('TYPEORM_SYNC'),
           }),
-        }), 
+        }),
+        
         MailerModule.forRootAsync({
           imports: [ConfigModule],
           inject: [ConfigService],

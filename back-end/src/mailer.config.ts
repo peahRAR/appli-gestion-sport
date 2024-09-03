068a949aeb19e377d85a243f49ea5eed6bece25e
peahRAR <padecallonne@gmail.com>
2024-09-03 20:23:50 +0200

    modification back mailer conf

--- back-end/src/mailer.config.ts
+++ back-end/src/mailer.config.ts
@@ -1,16 +1,12 @@
 import { MailerOptions } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { ConfigService } from '@nestjs/config';
-import { join } from 'path';
 
 export const mailerConfig = (configService: ConfigService): MailerOptions => {
   const smtpHost = configService.get<string>('SMTP_HOST');
   const smtpPort = configService.get<number>('SMTP_PORT');
   const smtpUser = configService.get<string>('SMTP_USER');
   const smtpPass = configService.get<string>('SMTP_PASS');
-
-  const templateDir = join(__dirname, '..', 'templates', 'email');
-  console.log('Template directory:', templateDir); // Log du chemin du template
 
 
   const options = {
@@ -31,7 +27,7 @@ export const mailerConfig = (configService: ConfigService): MailerOptions => {
       from: '"MMA-Association" <no-reply@mmabaisieux.fr>',
     },
     template: {
-      dir: templateDir,
+      dir: process.cwd() + '/templates/email/',
       adapter: new HandlebarsAdapter(),
       options: {
         strict: false,
